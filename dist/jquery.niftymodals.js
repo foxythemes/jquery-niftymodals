29aad584812c221d2c9752ed39a2e253b477dee6
MiguelMich <miguel_mich666@hotmail.com>
2016-11-21 16:38:50 -0500

    Bumped version to 1.3.1

--- dist/jquery.niftymodals.js
+++ dist/jquery.niftymodals.js
@@ -1,5 +1,5 @@
 /*!
- * jquery.niftymodals v1.3.0 (https://github.com/foxythemes/jquery-niftymodals)
+ * jquery.niftymodals v1.3.1 (https://github.com/foxythemes/jquery-niftymodals)
  * Copyright 2016 Codrops <www.codrops.com> and Foxy Themes 
  * Licensed under Codrops license http://tympanus.net/codrops/licensing/ 
  */
